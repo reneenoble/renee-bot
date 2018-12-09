6e7a6e3056e44f710037cdc1d411eabaaec34763
reneenoble <renee.noble@hotmail.com>
2018-12-09 12:35:13 +1100

    Basic bot

---
+++ renee_bot.py
@@ -0,0 +1,26 @@
+# Import the flask library
+from flask import Flask, request
+
+# Create your web server
+app = Flask(__name__)
+
+
+
+# When people visit the home page '/' use the hello_world function
+@app.route('/')
+def hello_world():
+    # Just a simple function that says Hello, World!
+    return 'Hello hello!'
+
+# You can message lol_bot via <your website>/lol
+@app.route('/lol')
+def lol_bot():
+    # Get the value of the 'text' query parameter
+    # request.args is a dictionary (cool!)
+    text = request.args.get('text')
+    # This bot lols at every command it gets sent!
+    return f'lol {text}'
+
+
+# Start the web server!
+app.run()
